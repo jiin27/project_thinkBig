13a3c8fcce3520f0f1008d072a9f0aba5d7950af
jiin27 <pjiin211@gmail.com>
2023-06-12 12:43:10 +0900

    개인project 1차

---
+++ Box.js
@@ -0,0 +1,22 @@
+//이미지 덮을 박스
+class Box{
+    constructor(container, width, height, x, y){
+        this.container=container;
+        this.div;
+        this.width=width;
+        this.height=height;
+        this.x=x;
+        this.y=y;
+
+        this.div=document.createElement("div");
+        this.div.style.width=this.width+"px";
+        this.div.style.height=this.height+"px";
+        this.div.style.background="whitesmoke";
+
+        this.div.style.position="absolute";
+        this.div.style.left=this.x+"px";
+        this.div.style.top=this.y+"px";
+
+        this.container.appendChild(this.div);
+    }
+}
