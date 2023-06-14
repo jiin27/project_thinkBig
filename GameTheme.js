05fd799a96252f1d06b144a25b3b8c8e3d226eab
jiin27 <pjiin211@gmail.com>
2023-06-14 16:53:10 +0900

    백업1차_setInterval오류

---
+++ GameTheme.js
@@ -0,0 +1,116 @@
+let gameThemePeople = [
+    {
+        "src": "./images/people/hongjinkyung.png",
+        "name": "홍진경"
+    },
+    {
+        "src": "./images/people/kimyuna.jpg",
+        "name": "김연아"
+    },
+    {
+        "src": "./images/people/josaeho.jpg",
+        "name": "조세호"
+    },
+    {
+        "src": "./images/people/leejaeyong.png",
+        "name": "이재용"
+    },
+    {
+        "src": "./images/people/lameelan.jpg",
+        "name": "라미란"
+    },
+    {
+        "src": "./images/people/sonheungmin.jpg",
+        "name": "손흥민"
+    },
+    {
+        "src": "./images/people/nayoungseok.png",
+        "name": "나영석"
+    },
+    {
+        "src": "./images/people/madongseok.jpg",
+        "name": "마동석"
+    },
+    {
+        "src": "./images/people/leeseongkyung.jpg",
+        "name": "이성경"
+    },
+    {
+        "src": "./images/people/bongjunho.jpg",
+        "name": "봉준호"
+    }
+
+]
+
+/*
+let peopleArray = [
+    ["./images/people/hongjinkyung.png", "홍진경"],
+    ["./images/people/kimyuna.jpg", "김연아"],
+    ["./images/people/josaeho.jpg", "조세호"],
+    ["./images/people/leejaeyong.png", "이재용"],
+    ["./images/people/lameelan.jpg", "라미란"],
+    ["./images/people/sonheungmin.jpg", "손흥민"],
+    ["./images/people/nayoungseok.png", "나영석"],
+    ["./images/people/madongseok.jpg", "마동석"],
+    ["./images/people/leeseongkyung.jpg", "이성경"],
+    ["./images/people/bongjunho.jpg", "봉준호"]
+];
+*/
+
+let gameThemeFood = [
+    {
+        "src": "./images/src/pancake.jpg",
+        "name": "팬케이크"
+    },
+    {
+        "src": "./images/src/hamburger.jpg",
+        "name": "햄버거"
+    },
+    {
+        "src": "./images/src/ramen.jpg",
+        "name": "라멘"
+    },
+    {
+        "src": "./images/src/potato.jpg",
+        "name": "감자튀김"
+    },
+    {
+        "src": "./images/src/sushi.jpg",
+        "name": "초밥"
+    },
+    {
+        "src": "./images/src/chickensoup.jpg",
+        "name": "삼계탕"
+    },
+    {
+        "src": "./images/src/tteokbokki.jpg",
+        "name": "떡볶이"
+    },
+    {
+        "src": "./images/src/coldnoodle.jpg",
+        "name": "냉면"
+    },
+    {
+        "src": "./images/src/hotteok.jpg",
+        "name": "호떡"
+    },
+    {
+        "src": "./images/src/pizza.jpg",
+        "name": "피자"
+    }
+];
+/*
+    let srcArray = [
+        ["./images/src/pancake.jpg", "팬케이크"],
+        ["./images/src/hamburger.jpg", "햄버거"],
+        ["./images/src/ramen.jpg", "라멘"],
+        ["./images/src/potato.jpg", "감자튀김"],
+        ["./images/src/sushi.jpg", "초밥"],
+        ["./images/src/chickensoup.jpg", "삼계탕"],
+        ["./images/src/tteokbokki.jpg", "떡볶이"],
+        ["./images/src/coldnoodle.jpg", "냉면"],
+        ["./images/src/hotteok.jpg", "호떡"],
+        ["./images/src/pizza.jpg", "피자"]
+    ];
+
+*/
