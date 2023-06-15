d9128b3a516cb9fa400ba85089f568976d1d71c8
jiin27 <pjiin211@gmail.com>
2023-06-15 15:49:40 +0900

    백업2차

---
+++ GameStage.js
@@ -0,0 +1,88 @@
+let stageArray = [
+    [
+        {
+            "src": "./images/people/hongjinkyung.png",
+            "name": "홍진경"
+        },
+        {
+            "src": "./images/people/kimyuna.jpg",
+            "name": "김연아"
+        },
+        {
+            "src": "./images/people/josaeho.jpg",
+            "name": "조세호"
+        },
+        {
+            "src": "./images/people/leejaeyong.png",
+            "name": "이재용"
+        },
+        {
+            "src": "./images/people/lameelan.jpg",
+            "name": "라미란"
+        },
+        {
+            "src": "./images/people/sonheungmin.jpg",
+            "name": "손흥민"
+        },
+        {
+            "src": "./images/people/nayoungseok.png",
+            "name": "나영석"
+        },
+        {
+            "src": "./images/people/madongseok.jpg",
+            "name": "마동석"
+        },
+        {
+            "src": "./images/people/leeseongkyung.jpg",
+            "name": "이성경"
+        },
+        {
+            "src": "./images/people/bongjunho.jpg",
+            "name": "봉준호"
+        }
+
+    ],
+
+    [
+        {
+            "src": "./images/src/pancake.jpg",
+            "name": "팬케이크"
+        },
+        {
+            "src": "./images/src/hamburger.jpg",
+            "name": "햄버거"
+        },
+        {
+            "src": "./images/src/ramen.jpg",
+            "name": "라멘"
+        },
+        {
+            "src": "./images/src/potato.jpg",
+            "name": "감자튀김"
+        },
+        {
+            "src": "./images/src/sushi.jpg",
+            "name": "초밥"
+        },
+        {
+            "src": "./images/src/chickensoup.jpg",
+            "name": "삼계탕"
+        },
+        {
+            "src": "./images/src/tteokbokki.jpg",
+            "name": "떡볶이"
+        },
+        {
+            "src": "./images/src/coldnoodle.jpg",
+            "name": "냉면"
+        },
+        {
+            "src": "./images/src/hotteok.jpg",
+            "name": "호떡"
+        },
+        {
+            "src": "./images/src/pizza.jpg",
+            "name": "피자"
+        }
+    ]
+];
